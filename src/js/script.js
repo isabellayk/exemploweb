9d36c6f7e6c642c1fc471d679fc7d03c3a0ba6fe
Isabella Yogui <isabellayk13@gmail.com>
2026-03-30 09:15:49 -0300

    Feat: Declarações e variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,46 @@
-alert("Hello World !!!")
+// Declarações e variaveis
+// var, let const
+
+var nome = "Fiap";
+console.log(nome);
+
+var nome = "Teste";
+console.log(nome);
+
+if (true){
+    let apelido = "Fiapinho"
+    console.log(apelido);
+}
+
+const taxaJuros = 0.05;
+console.log(taxaJuros);
+// taxaJuros = 1.5;
+
+var exemplo = "ola-dev-var";
+console.log(typeof exemplo) ;
+
+let exemplo1 = "ola-dev-let";
+console.log(exemplo1);
+
+const exemplo2 = "ola-dev-const";
+console.log(exemplo2);
+
+let exemplo3 = 10;
+console.log(typeof exemplo3);
+
+let exemplo4= true;
+console.log(exemplo4);
+
+let exemplo5={nome1:"teste"};
+console.log(exemplo5);
+
+let exemplo6 = ["java", "C#", "PHP"];
+    console.log(exemplo6);
+
+//valor undefined
+let exemplo7;
+console.log(exemplo7);
+
+//valor vazio
+let exemplo8 = null;
+console.log(exemplo8);
